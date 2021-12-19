882fd7c312ffd88a701c2356a9d92172b2baba44
Aaron Shames <aaron@1coin.io>
2021-12-19 04:10:43 -0500

    change farm timing

--- src/components/Disclaimer/Disclaimer.js
+++ src/components/Disclaimer/Disclaimer.js
@@ -14,7 +14,18 @@ const Disclaimer = () => {
 
   return (
     <Grid container item className={classes.root} justifyContent="center">
-      <Typography className={classes.disclaimer}>{t('Disclaimer')}</Typography>
+      <Typography className={classes.disclaimer}>
+        {t('Disclaimer')}
+        <br />
+        <br />
+        BOMB was not effected by any exploits.{' '}
+        <a
+          target="_blank"
+          href="https://medium.com/@bombbshare/dec-18-quick-update-regarding-grim-exploit-4f1a0c989fba"
+        >
+          Read more here
+        </a>
+      </Typography>
     </Grid>
   );
 };

7375aea1872865281e5c5adae0025706e7681950
Aaron Shames <aaron@1coin.io>
2022-06-20 17:02:32 -0400

    add filter back

--- scripts/config.js
+++ scripts/config.js
@@ -12,24 +12,8 @@ import { bscPools } from '../src/features/configure/vault/bsc_pools.js';
 export const chainPools = {
   bsc: bscPools,
   // heco: hecoPools,
-  // avax: avalanchePools,
-  // polygon: polygonPools,
-  // fantom: fantomPools,
-  // one: harmonyPools,
-  // arbitrum: arbitrumPools,
-  // celo: celoPools,
-  // moonriver: moonriverPools,
-  // cronos: cronosPools,
 };
 export const chainRpcs = {
   bsc: process.env.BSC_RPC || 'https://rpc.ankr.com/bsc',
   // heco: process.env.HECO_RPC || 'https://http-mainnet.hecochain.com',
-  // avax: process.env.AVAX_RPC || 'https://api.avax.network/ext/bc/C/rpc',
-  // polygon: process.env.POLYGON_RPC || 'https://polygon-rpc.com',
-  // fantom: process.env.FANTOM_RPC || 'https://rpc.ftm.tools/',
-  // one: process.env.HARMONY_RPC || 'https://api.s0.t.hmny.io/',
-  // arbitrum: process.env.ARBITRUM_RPC || 'https://arb1.arbitrum.io/rpc',
-  // celo: process.env.CELO_RPC || 'https://forno.celo.org',
-  // moonriver: process.env.MOONRIVER_RPC || 'https://rpc.moonriver.moonbeam.network',
-  // cronos: process.env.CRONOS_RPC || 'https://evm-cronos.crypto.org',
 };
